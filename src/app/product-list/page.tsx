0796da22d04cc5e9233d7f256243672efde8d0c3
Hoang Hy <hoanghy01122003@gmail.com>
2024-10-25 00:58:11 +0700

    feat: add product list page

--- src/app/product-list/page.tsx
+++ src/app/product-list/page.tsx
@@ -1,4 +1,17 @@
+import ProductItem from "@/components/ProductItem";
+import { getProductListAPI } from "@/services/api/product/product-list";
 
-export default function Page() {
-    
+export default async function Page() {
+	const productResponse = await getProductListAPI();
+	const productList = productResponse.products;
+
+	return (
+		<div className=" px-24 pt-10">
+			<div className=" grid grid-cols-6 gap-5">
+				{productList.map((product) => (
+					<ProductItem key={product.id} product={product} />
+				))}
+			</div>
+		</div>
+	);
 }
